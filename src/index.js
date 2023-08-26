e2deb54d302d093664767235e37be7b191b95e49
Tetiana-Vakhovska <87013046+Tetiana-Vakhovska@users.noreply.github.com>
2023-08-26 21:31:27 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 import {soshForm} from 'components/form';
-
+let soshForm
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
