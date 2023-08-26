b8006d88d5f55d133a05e9fbd73cb84627954597
Tetiana-Vakhovska <87013046+Tetiana-Vakhovska@users.noreply.github.com>
2023-08-26 21:59:34 +0300

    Update form.jsx

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,11 +1,11 @@
  export const soshForm=()=>{
     return <div class="profile">
     <div class="description">
-      <img
-        src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-        alt="User avatar"
-        class="avatar"
-      />
+     
+      <img src={"https://cdn-icons-png.flaticon.com/512/1077/1077012.png"} 
+      alt="User avatar" width="640" />
+        
+    
 
 <CardWrapper>
       <p>{$name}</p>
